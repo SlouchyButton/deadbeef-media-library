41e9d684526acdc40839a1f75eb83eacaa16efbd
Aleš Nezbeda <ales.nezbeda@kwindu.eu>
2022-10-12 10:46:54 +0200

    Re-enable fixed search after it was broken by refactor

--- include/gui/filebrowserfilter.hpp
+++ include/gui/filebrowserfilter.hpp
@@ -3,18 +3,21 @@
 #include <gtkmm.h>
 
 #include "treefilebrowser.hpp"
+#include "modelcolumns.hpp"
 
 class FilebrowserFilter : public Gtk::TreeModelFilter
 {
 public:
     FilebrowserFilter(Glib::RefPtr<Gtk::TreeModel> model);
     ~FilebrowserFilter();
-    static Glib::RefPtr<FilebrowserFilter> create(Glib::RefPtr<TreeFilebrowser> treefb);
+    static Glib::RefPtr<FilebrowserFilter> create(Glib::RefPtr<Gtk::ListStore> treefb);
     void setNeedle(std::string newNeedle);
     void setModel(TreeFilebrowser* treefb);
 private:
     std::string mNeedle;
     TreeFilebrowser* mTreeFilebrowser;
+    ModelColumns mModelColumns;
+
 
     bool filter_func(Gtk::TreeIter iter);
 };
--- src/gui/filebrowserfilter.cpp
+++ src/gui/filebrowserfilter.cpp
@@ -1,40 +1,53 @@
 #include "filebrowserfilter.hpp"
+#include "medialibrary.hpp"
 
 #include <boost/algorithm/string.hpp>
 
+#include <iostream>
+
 FilebrowserFilter::FilebrowserFilter(Glib::RefPtr<Gtk::TreeModel> model) :
 Gtk::TreeModelFilter(model) {
+    this->set_visible_func(mem_fun(*this, &FilebrowserFilter::filter_func));
 }
 
-Glib::RefPtr<FilebrowserFilter> FilebrowserFilter::create(Glib::RefPtr<TreeFilebrowser> treefb) {
+Glib::RefPtr<FilebrowserFilter> FilebrowserFilter::create(Glib::RefPtr<Gtk::ListStore> treefb) {
     return Glib::RefPtr<FilebrowserFilter>(new FilebrowserFilter(treefb));
 }
 
 void FilebrowserFilter::setNeedle(std::string newNeedle) {
+    std::cout << "Lowering needle: " << newNeedle << std::endl;
     this->mNeedle = boost::to_lower_copy(newNeedle);
-    this->mTreeFilebrowser->setNeedleState(this->mNeedle.empty());
+    std::cout << "Needle: " << this->mNeedle << std::endl;
     this->refilter();
 }
 
 void FilebrowserFilter::setModel(TreeFilebrowser* treefb) {
     this->mTreeFilebrowser = treefb;
-    this->set_visible_func(mem_fun(*this, &FilebrowserFilter::filter_func));
 }
 
 bool FilebrowserFilter::filter_func(Gtk::TreeIter iter) {
-    if (!iter->children().empty()) {
-        // Check children, this is not very efficient, but the only way I could think of
-        auto child = iter->children().end();
-        do {
-            child--;
-            if (this->filter_func(child)) {
-                return true;
-            }
-        } while (child != iter->children().begin());
-        return false;
+    std::cout << "Filtering" << std::endl;
+    if (this->mNeedle.empty()) {
+        return true;
+    }
+    std::string currentTitle = boost::to_lower_copy(iter->get_value(mModelColumns.mColumnTitle));
+    std::string currentSubtitle = boost::to_lower_copy(iter->get_value(mModelColumns.mColumnSubtitle));
+    Album* currentAlbum = iter->get_value(mModelColumns.mColumnAlbumPointer);
+
+    if (currentTitle.find(this->mNeedle) != std::string::npos ||
+        currentSubtitle.find(this->mNeedle) != std::string::npos) {
+        return true;
     }
 
-    return (this->mNeedle.empty() || boost::algorithm::contains(boost::to_lower_copy(iter->get_value(this->mTreeFilebrowser->mModelColumns.mColumnTitle)), this->mNeedle));
+    std::string currentMediaFileTitle;
+    for (auto mediaFile : currentAlbum->MediaFiles) {
+        currentMediaFileTitle = boost::to_lower_copy(mediaFile->Title);
+        if (currentMediaFileTitle.find(this->mNeedle) != std::string::npos) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 FilebrowserFilter::~FilebrowserFilter() {
